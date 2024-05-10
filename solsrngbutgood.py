2c67b89dcbd39d2b901654a656558b3ac2d7da23
TrazNarf <85917813+TrazNarf@users.noreply.github.com>
2024-05-09 22:04:27 -0700

    the game
    wow

---
+++ solsrngbutgood.py
@@ -0,0 +1,46 @@
+import random 
+import time
+
+loop = True
+
+print("hello welcome to practically sol's rng")
+
+while loop == True:
+    funnynumber = random.randint(1, 1000)
+    if funnynumber == (1):
+        print("you got the funnyest number")
+        time.sleep(1)
+    if funnynumber == (276):
+        print("wow good job you did an rng")
+        time.sleep(1)
+    if funnynumber == (215):
+        print("that's an oddly specific number")
+        time.sleep(1)
+    if funnynumber == (69):
+        print("you got the really cool aura!!!")
+        time.sleep(1)
+    if funnynumber == (1000):
+        print("you got 1000!")
+        time.sleep(1)
+    if 500 <= funnynumber <= 999:
+        time.sleep(1)
+        print("you got the really common one")
+        time.sleep(1)
+    if 2 <= funnynumber <= 68:
+        print("this one is kinda rare!")
+        time.sleep(1)
+    if 68 <= funnynumber <= 214:
+        print("this one is not that common but still common")
+        time.sleep(1)
+    if 216 <= funnynumber <= 275:
+        print("really rare and specific but not 1 in 1000")
+        time.sleep(1)
+    if 277 <= funnynumber <= 419:
+        print("big majority of the numbers")
+        time.sleep(1)
+    if funnynumber == (420):
+        print("haha funny number")
+        time.sleep(1)
+    if 421 <= funnynumber <= 499:
+        print("im done with this")
+        time.sleep(1)
