b52a6ac3095d9de8e5d12f7b8a47f62f4092e016
TrazNarf <85917813+TrazNarf@users.noreply.github.com>
2024-05-10 20:09:15 -0700

    updated the game
    added a gui and cleaned u[p the code

--- solsrngbutgood.py
+++ solsrngbutgood.py
@@ -1,46 +1,41 @@
-import random 
+from tkinter import *
+import random
 import time
 
-loop = True
+root = Tk()
 
-print("hello welcome to practically sol's rng")
+rngdevice = Label(root)
+rngdevice.pack()
 
-while loop == True:
+def solsrng():
     funnynumber = random.randint(1, 1000)
     if funnynumber == (1):
-        print("you got the funnyest number")
-        time.sleep(1)
+        rngoutput = "you got the funnyest number"
     if funnynumber == (276):
-        print("wow good job you did an rng")
-        time.sleep(1)
+        rngoutput = "wow good job you did an rng"
     if funnynumber == (215):
-        print("that's an oddly specific number")
-        time.sleep(1)
+        rngoutput = "that's an oddly specific number"
     if funnynumber == (69):
-        print("you got the really cool aura!!!")
-        time.sleep(1)
+        rngoutput = "you got the really cool aura!!!"
     if funnynumber == (1000):
-        print("you got 1000!")
-        time.sleep(1)
+        rngoutput = "you got 1000!"
     if 500 <= funnynumber <= 999:
-        time.sleep(1)
-        print("you got the really common one")
-        time.sleep(1)
+        rngoutput = "you got the really common one"
     if 2 <= funnynumber <= 68:
-        print("this one is kinda rare!")
-        time.sleep(1)
+        rngoutput = "this one is kinda rare!"
     if 68 <= funnynumber <= 214:
-        print("this one is not that common but still common")
-        time.sleep(1)
+        rngoutput = "this one is not that common but still common"
     if 216 <= funnynumber <= 275:
-        print("really rare and specific but not 1 in 1000")
-        time.sleep(1)
+        rngoutput = "really rare and specific but not 1 in 1000"
     if 277 <= funnynumber <= 419:
-        print("big majority of the numbers")
-        time.sleep(1)
+        rngoutput = "big majority of the numbers"
     if funnynumber == (420):
-        print("haha funny number")
-        time.sleep(1)
+        rngoutput = "haha funny number"
     if 421 <= funnynumber <= 499:
-        print("im done with this")
-        time.sleep(1)
+        rngoutput = "im done with this"
+    rngdevice.config(text=rngoutput)
+    root.after(1000, solsrng)
+    
+solsrng()
+
+root.mainloop()
